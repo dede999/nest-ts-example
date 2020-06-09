bf2593816f4ca517ea07c0e5fae44a578119183a
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-06-09 16:53:06 -0300

    Implementing Task scheduling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 import { RecipeModule } from './recipe/recipe.module';
+import { CronTaskService } from './cron-task/cron-task.service';
+
 
 @Module({
   imports: [
-    RecipeModule,
+    RecipeModule, ScheduleModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/recipe-app',
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronTaskService],
 })
 export class AppModule {}

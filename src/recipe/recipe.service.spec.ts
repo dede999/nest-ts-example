a28d0d1c6ce87ec84aa62816a8cc0ccd68336b49
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-09-11 01:05:20 -0300

    Complete test for recipe.service.spec.ts
    Besides the tests, I managed to do it with less
    repeated code, thanks to Jay (jmcdo29) that helped
    me a lot showing his code.
    
    After completing my issues, I'll probably try to
    improve these tests

--- src/recipe/recipe.service.spec.ts
+++ src/recipe/recipe.service.spec.ts
@@ -2,92 +2,138 @@ import { RecipeService } from "./recipe.service";
 import { Recipe, recipeFactory } from "./interfaces/recipe.interface";
 import { Test } from "@nestjs/testing";
 import { Model } from "mongoose";
+import { random } from "faker";
 import { getModelToken } from "@nestjs/mongoose";
-import { exampleWithoutExec } from "../../test/shared/mongoose.examples";
 
-const { fn, clearAllMocks } = jest;
+const { fn, clearAllMocks, mock } = jest;
+const { alphaNumeric } = random;
 const recipe = recipeFactory.getOne();
 const someRecipes = recipeFactory.getSome(3);
 
-describe("RecipeService", () => {
-  let service: RecipeService;
+describe("RecipeService", function() {
   let model: Model<Recipe>;
+  let service: RecipeService;
+  const updateArgs = [alphaNumeric(6), recipe, { new: true }];
 
-  beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        RecipeService,
-        {
-          provide: getModelToken("Recipe"),
-          useValue: {},
-        },
-      ],
-    }).compile();
-    //
-    service = module.get<RecipeService>(RecipeService);
-    model = module.get<Model<Recipe>>(getModelToken("Recipe"));
-  });
+  describe.each`
+    functionName      | dbMethod               | mockData       | args               | expected
+    ${"addRecipe"}    | ${"create"}            | ${someRecipes} | ${someRecipes}     | ${someRecipes}
+    ${"updateRecipe"} | ${"findByIdAndUpdate"} | ${recipe}      | ${updateArgs}      | ${recipe}
+    ${"deleteRecipe"} | ${"findByIdAndRemove"} | ${recipe}      | ${alphaNumeric(6)} | ${recipe}
+  `("$functionName", function({
+    functionName,
+    dbMethod,
+    mockData,
+    args,
+    expected,
+  }) {
+    beforeEach(async function() {
+      const module = await Test.createTestingModule({
+        providers: [
+          RecipeService,
+          {
+            provide: getModelToken("Recipe"),
+            useValue: {},
+          },
+        ],
+      }).compile();
 
-  afterAll(() => clearAllMocks());
+      service = module.get<RecipeService>(RecipeService);
+      model = module.get<Model<Recipe>>(getModelToken("Recipe"));
 
-  describe("filterRecipes", () => {
-    beforeEach(() => {
-      service.allRecipes = fn().mockResolvedValue(someRecipes);
+      model[dbMethod] = fn().mockResolvedValue(mockData);
     });
 
-    it("should return all recipes with no given filters", async () => {
-      expect(await service.filterRecipes()).toEqual(someRecipes);
+    it("should return the expected value for the method", async function() {
+      expect(await service[functionName](...args)).toEqual(expected);
     });
 
-    it("should return all recipes that has an specific word", async () => {
-      const filterWithSearch = { search: someRecipes[0].title.split(" ")[0] };
-      const result = await service.filterRecipes(filterWithSearch);
+    it(`should have called model.${dbMethod}`, async function() {
+      await service[functionName](...args);
 
-      expect(result.length).toBeLessThan(3);
-    });
-
-    it("should return all recipes with an specific category", async () => {
-      const filterWithCategory = { category: someRecipes[1].category };
-      const result = await service.filterRecipes(filterWithCategory);
-
-      expect(result.length).toBeLessThan(3);
-    });
-
-    it("should have called allRecipes method", async () => {
-      await service.filterRecipes();
-
-      expect(service.allRecipes).toHaveBeenCalled();
-    });
-  });
-
-  describe("allRecipes", () => {
-    beforeEach(() => {
-      model.find = fn().mockReturnValue({
-        exec: fn().mockResolvedValue(someRecipes),
-      });
-    });
-
-    it("should return the array of recipes", async () => {
-      expect(await service.allRecipes()).toEqual(someRecipes);
-    });
-
-    it("should have called find", async () => {
-      await service.allRecipes();
-
-      expect(model.find).toHaveBeenCalled();
+      expect(model[dbMethod]).toHaveBeenCalled();
     });
   });
 
   describe.each`
-    functionName   | mockData  | args      | expected
-    ${"addRecipe"} | ${recipe} | ${recipe} | ${recipe}
-  `("$functionName", ({ functionName, mockData, args, expected }) => {
-    exampleWithoutExec(
-      service[functionName],
-      "create",
-      mockData,
+    functionName    | dbMethod      | mockData       | args               | expected
+    ${"allRecipes"} | ${"find"}     | ${someRecipes} | ${[]}              | ${someRecipes}
+    ${"aRecipe"}    | ${"findById"} | ${recipe}      | ${alphaNumeric(6)} | ${recipe}
+  `("$functionName", function({
+    functionName,
+    dbMethod,
+    mockData,
+    args,
+    expected,
+  }) {
+    beforeEach(async function() {
+      const module = await Test.createTestingModule({
+        providers: [
+          RecipeService,
+          {
+            provide: getModelToken("Recipe"),
+            useValue: {},
+          },
+        ],
+      }).compile();
+
+      service = module.get<RecipeService>(RecipeService);
+      model = module.get<Model<Recipe>>(getModelToken("Recipe"));
+
+      model[dbMethod] = fn().mockReturnValue({
+        exec: fn().mockResolvedValue(mockData),
+      });
+    });
+
+    it("should return the expected value for the method", async function() {
+      expect(await service[functionName](...args)).toEqual(expected);
+    });
+
+    it(`should have called model.${dbMethod}`, async function() {
+      await service[functionName](...args);
+
+      expect(model[dbMethod]).toHaveBeenCalled();
+    });
+  });
+
+  describe("filterRecipes", function() {
+    const titleFilter = { search: someRecipes[0].title };
+    const categoryFilter = { category: someRecipes[1].category };
+
+    beforeEach(async function() {
+      const module = await Test.createTestingModule({
+        providers: [
+          RecipeService,
+          {
+            provide: getModelToken("Recipe"),
+            useValue: {},
+          },
+        ],
+      }).compile();
+
+      service = module.get<RecipeService>(RecipeService);
+
+      service.allRecipes = fn().mockResolvedValue(someRecipes);
+    });
+
+    it("should call allRecipes method", async function() {
+      await service.filterRecipes();
+
+      expect(service.allRecipes).toHaveBeenCalled();
+    });
+
+    it.each`
+      title                       | args              | expectation
+      ${"no filters are applied"} | ${{}}             | ${"toEqual"}
+      ${"searching the title"}    | ${titleFilter}    | ${"toBeLessThan"}
+      ${"searching a category"}   | ${categoryFilter} | ${"toBeLessThan"}
+    `("when $title, result.length is expected $expectation 3", async function({
       args,
-      expected,
-    );
+      expectation,
+    }) {
+      const result = await service.filterRecipes(args);
+
+      expect(result.length)[expectation](3);
+    });
   });
 });
